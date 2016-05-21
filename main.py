53f26d4e456b43f3fd71a4736f7d6da7814a5230
quinten2twee <quintentupker@yahoo.com>
2016-05-21 14:00:18 -0500

    This is a test.

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("The world is blue.")
+import kivy
+kivy.require('1.9.1')
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+
+
+class Pong(App):
+    def build(self):
+        self.load_kv(filename="main.kv")
+
+if __name__ == '__main__':
+    Pong().run()
