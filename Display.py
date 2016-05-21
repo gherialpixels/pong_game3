359b3e533c39093d0d4238bff64054fda2c14705
GherialPixel <gherialpixels@gmail.com>
2016-05-21 19:43:22 +0200

    Hi

--- Display.py
+++ Display.py
@@ -1,5 +1,23 @@
 
+from kivy.app import App
+
+from kivy.uix.scatter import Scatter
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.textinput import TextInput
+
+class ScatterTextWidget(BoxLayout):
+    pass
+
+class PingPongApp(App):
+    def build(self):
+        return ScatterTextWidget()
+
 def foo():
     print "Hello"
 
 foo()
+
+if __name__ == "__main__":
+    PingPongApp().run()
