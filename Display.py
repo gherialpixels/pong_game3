4054f13a7901ae9a2a96cfc4eb7cd00c0a0601a1
GherialPixel <gherialpixels@gmail.com>
2016-05-21 22:43:17 +0200

    Hi

--- Display.py
+++ Display.py
@@ -7,17 +7,24 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.textinput import TextInput
 
+from kivy.properties import ListProperty
+
+import random
+
 class ScatterTextWidget(BoxLayout):
-    pass
+
+    text_colour = ListProperty([1, 0, 0, 1])
+
+    def change_label_colour(self, *args):
+        # relating Kivy language to Python
+        colour = [random.random() for i in xrange(3)] + [1]
+        # completely general way to refer to Kivy labels
+
+        self.text_colour = colour
 
 class PingPongApp(App):
     def build(self):
         return ScatterTextWidget()
-
-def foo():
-    print "Hello"
-
-foo()
 
 if __name__ == "__main__":
     PingPongApp().run()
