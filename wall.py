26d56e70668637b0d6b1d1bf5e5340a3c98cc6cf
quinten2twee <quintentupker@yahoo.com>
2016-05-28 11:13:01 -0500

    minor tweaks

--- wall.py
+++ wall.py
@@ -6,5 +6,10 @@ class Wall(Widget):
         super(Wall, self).__init__(self, **kwargs)
         self.y = -1
         self.x = 0
-        self.size_hint_x = self.get_root_window().system_size #works?
+        self.size[0] = self.get_parent_window().system_size[0]
+        self.size[1] = 1
+
+class BounceWall(Wall):
+    def __init__(self, **kwargs):
+        super(Wall, self).__init__(self, **kwargs)
 
